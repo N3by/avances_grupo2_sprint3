bd5e1eb76278ca9a509718d437ec9eacde9369c8
Juan Mojica <mojicajuan1104@gmail.com>
2022-11-12 16:46:36 -0500

    Activity configuración terminada

--- app/src/main/java/com/example/myparkea/Configuracion.kt
+++ app/src/main/java/com/example/myparkea/Configuracion.kt
@@ -2,10 +2,24 @@ package com.example.myparkea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 
 class Configuracion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
+
+        val switch = findViewById<Switch>(R.id.dark_mode)
+        switch.setOnCheckedChangeListener { _, _ ->
+            if (switch.isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switch.text = "Desactivar modo oscuro: "
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switch.text = "Activar modo oscuro: "
+            }
+        }
     }
 }
